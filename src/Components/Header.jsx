7decdf05008fc20e4f11c0794b0287848337be4d
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-04-05 00:46:24 +0600

    buy now btn updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,8 +10,8 @@ const Header = () => {
        <nav className={ `flex gap-6 md:gap-16 justify-between md:justify-end items-center absolute w-full md:static flex-col md:flex-row transition delay-200 ease-in-out  z-10 bg-black ${menuOpen ? 'top-[44px] left-0 w-full pb-5' : 'top-[-300px]'}`}>
            <Link to='/'>Home</Link>
            <Link to='/reviews'>Reviews</Link>
-           <Link to='/blog'>Blog</Link>
            <Link to='/about'>About</Link>
+           <Link to='/blog'>Blog</Link>
            <Link to='/dashboard'>DashBoard</Link>
        </nav>
        <div onClick={() => setMenuOpen(!menuOpen)} className=' w-6 md:hidden transition delay-75'>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import appleTv from "../assets/image/apple-tv.png";
+import {ShoppingCartIcon} from "@heroicons/react/solid"
 
 const Home = () => {
   return (
@@ -20,11 +21,15 @@ const Home = () => {
             Netflix, and Disney+, as well as live channels from Hulu, YouTube
             TV, and Sling TV.⁵
           </p>
+        <div className="flex w-36 justify-center text-white items-center cursor-pointer mt-5 hover:bg-blue-500 rounded px-3 py-2 bg-blue-600">
+        <button>Buy now </button>
+        <ShoppingCartIcon className="w-6 ml-3"/>
+      </div>
         </div>
         <div className="right-side md:w-[50%] md:order-2">
           <img className="w-full" src={appleTv} alt="iphone 13 pro" />
         </div>
-      </div>
+        </div>
     </>
   );
 };
